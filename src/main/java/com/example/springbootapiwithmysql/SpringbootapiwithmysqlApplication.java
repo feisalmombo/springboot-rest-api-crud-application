61662b9bd5f3de03dabad361c2246af3171b741e
feisalmombo <feisalmombo29@gmail.com>
2024-07-23 11:32:34 +0300

    Initial commit

---
+++ src/main/java/com/example/springbootapiwithmysql/SpringbootapiwithmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapiwithmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapiwithmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapiwithmysqlApplication.class, args);
+	}
+
+}
