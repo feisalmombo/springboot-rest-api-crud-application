a5349e203ddd66bc7e8095ae33ee58e7d99996fb
feisalmombo <feisalmombo29@gmail.com>
2024-07-23 17:58:59 +0300

    WIP

---
+++ src/main/java/com/example/springbootapiwithmysql/CourseService.java
@@ -0,0 +1,13 @@
+package com.example.springbootapiwithmysql;
+
+import java.util.List;
+
+public interface CourseService {
+
+    public String upsert(Course course);
+
+    public Course getById(Integer cid);
+
+    public List<Course> getAllCourses();
+
+}
