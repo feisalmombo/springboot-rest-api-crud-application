f4934116312cce1c7782e6af0bd4943f6b5f1bc4
feisalmombo <feisalmombo29@gmail.com>
2024-07-25 17:02:30 +0300

    WIP

--- src/main/java/com/example/springbootapiwithmysql/CourseRestController.java
+++ src/main/java/com/example/springbootapiwithmysql/CourseRestController.java
@@ -1,5 +1,21 @@
 package com.example.springbootapiwithmysql;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CourseRestController {
-    
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping("/course")
+    public ResponseEntity<String> createCourse(@RequestBody Course course) {
+        String status = courseService.upsert(course);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
 }
