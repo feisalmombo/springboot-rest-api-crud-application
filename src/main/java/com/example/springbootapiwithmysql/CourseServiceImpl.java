1d1650ffd2f36e06c075668db74f4b292ac609fb
feisalmombo <feisalmombo29@gmail.com>
2024-07-25 16:39:54 +0300

    fix bugs

--- src/main/java/com/example/springbootapiwithmysql/CourseServiceImpl.java
+++ src/main/java/com/example/springbootapiwithmysql/CourseServiceImpl.java
@@ -1,27 +1,51 @@
 package com.example.springbootapiwithmysql;
 
 import java.util.List;
+import java.util.Optional;
 
-public class CourseServiceImpl implements CourseService{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    private CourseRepository courseRepo;
 
     @Override
-    public String deleteById(Integer cid) {
-        return null;
+    public String upsert(Course course) {
+
+        courseRepo.save(course);
+
+        return "Success";
     }
 
     @Override
     public Course getById(Integer cid) {
-        return null;
-    }
 
-    @Override
-    public String upsert(Course course) {
+        Optional<Course> findById = courseRepo.findById(cid);
+
+        if (findById.isPresent()) {
+            return findById.get();
+        }
         return null;
     }
 
     @Override
     public List<Course> getAllCourses() {
-        return null;
+        return courseRepo.findAll();
     }
-    
+
+    @Override
+    public String deleteById(Integer cid) {
+
+        if (courseRepo.existsById(cid)) {
+            courseRepo.deleteById(cid);
+
+            return "Delete Success";
+        } else {
+            return "No Record Found";
+        }
+    }
+
 }
